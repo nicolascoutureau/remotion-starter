e1ab46dd7b78f8cd6657d092d5f2a244dce72ee0
Nicolas Coutureau <nicolas.coutureau@gmail.com>
2025-07-09 16:30:15 +0200

    Update package.json and package-lock.json to synchronize @remotion dependencies to version 4.0.323 and refactor HelloWorld component by removing comments for cleaner code.

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -9,13 +9,11 @@ export const HelloWorld: React.FC = () => {
   const frame = useCurrentFrame();
   const { durationInFrames } = useVideoConfig();
 
-  // Fade out the animation at the end
   const opacity = interpolate(frame, [0, durationInFrames], [0, 1], {
     extrapolateLeft: "clamp",
     extrapolateRight: "clamp",
   });
 
-  // A <AbsoluteFill> is just a absolutely positioned <div>!
   return (
     <AbsoluteFill className="flex items-center justify-center bg-white">
       <h1
