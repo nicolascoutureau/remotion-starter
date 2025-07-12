7b3ef1585609e1e5d1bf0bff5e53c73b0aa6318c
Nicolas Coutureau <nicolas.coutureau@gmail.com>
2025-07-12 19:14:35 +0200

    Refactor useGsapTimeline hook to use memoized timeline factory for improved performance and dependency management.

--- src/hooks/useGsapTimeline.tsx
+++ src/hooks/useGsapTimeline.tsx
@@ -1,22 +1,26 @@
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useCurrentFrame, useVideoConfig } from "remotion";
 
 export const useGsapTimeline = <T extends HTMLElement>(
   gsapTimelineFactory: () => gsap.core.Timeline,
+  deps: React.DependencyList = [],
 ) => {
   const animationScopeRef = useRef<T>(null);
   const timelineRef = useRef<gsap.core.Timeline>(null);
   const frame = useCurrentFrame();
   const { fps } = useVideoConfig();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const memoizedTimelineFactory = useCallback(gsapTimelineFactory, deps);
+
   useEffect(() => {
     const ctx = gsap.context(() => {
-      timelineRef.current = gsapTimelineFactory();
+      timelineRef.current = memoizedTimelineFactory();
       timelineRef.current.pause();
     }, animationScopeRef);
     return () => ctx.revert();
-  }, [gsapTimelineFactory]);
+  }, [memoizedTimelineFactory]);
 
   useEffect(() => {
     if (timelineRef.current) {
